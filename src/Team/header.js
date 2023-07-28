30b2b3c4c9b68acf31428cd08511839009cc2742
Rakib Bin Abdur Rahim <rakibattaridib@gmail.com>
2023-07-28 22:22:54 +0600

    Delete src directory

--- src/Team/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header({ currTeam, curMemberCount }) {
-    return (
-        <div className="header">
-            <h2> Team member Allocation</h2>
-        </div>
-    )
-}
--- src/footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return (
-        <div className="footer">
-            <p>Team Member Allocation App - {new Date().getFullYear()}</p>
-        </div>
-    );
-};
